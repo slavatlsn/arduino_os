4828c74731b1f1c6b0f6be3190fb853db4dfddc2
slavatlsn <93151297+slavatlsn@users.noreply.github.com>
2022-05-26 13:46:06 +0300

    Create Os.h

---
+++ Os.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Arduino.h"
+
+class Os
+{
+  public:
+  Os();
+  int n;
+  void** (**apps)();
+  String* app_list;
+  void app(String name, void(*pointer)());
+  void run_app(String name);
+};
