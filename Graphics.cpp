a5aee78670ed4b2a6745a965563f9d127ef9f4fa
slavatlsn <93151297+slavatlsn@users.noreply.github.com>
2023-01-21 09:16:34 +0300

    Update Graphics.cpp

--- Graphics.cpp
+++ Graphics.cpp
@@ -1,4 +1,8 @@
 #include <Graphics.h>
+#include <LiquidCrystal>
+#include <Arduino.h>
+
+//----------------------------------------------------------------------------------------------------------------
 
 ProgressBar::ProgressBar(int x_1, int y_1, int x_2, int y_2)
 {
